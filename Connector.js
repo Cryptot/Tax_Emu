4e3820c43c803e0727d266aaf48a0aab9c76c293
eh1998 <>
2018-08-22 23:43:28 +0200

    fixed ticker

--- Connector.js
+++ Connector.js
@@ -510,6 +510,7 @@ function TickerData(snapshotData) {
 TickerData.prototype.maxLength = 25;
 
 TickerData.prototype.update = function (updateData) {
+    updateData = updateData[0];
     let newRow = {
         timestamp: +new Date(),
         frr: updateData[0],
@@ -667,11 +668,43 @@ let Connector = {
         this.ws.onopen = function () {
 
             let element = document.getElementById("ask");
-            element.addEventListener("data", function (event) {
+            /*element.addEventListener("data", function (event) {
                 console.log(event.detail.data);
+                const data = event.detail.data;
+                let col = [];
+                for (let i = 0; i < data.length; i++) {
+                    for (let key in data[i]) {
+                        if (col.indexOf(key) === -1) {
+                            col.push(key);
+                        }
+                    }
+                }
+                let table = document.createElement("table");
+                let tr = table.insertRow(-1);
 
+                for (let i = 0; i < col.length; i++) {
+                    let th = document.createElement("th");      // TABLE HEADER.
+                    th.innerHTML = col[i];
+                    tr.appendChild(th);
+                }
+
+                for (let i = 0; i < data.length; i++) {
+
+                    tr = table.insertRow(-1);
+
+                    for (let j = 0; j < col.length; j++) {
+                        let tabCell = tr.insertCell(-1);
+                        tabCell.innerHTML = data[i][col[j]];
+                    }
+                }
+                this.innerHTML = "";
+                this.appendChild(table);
+            });*/
+            element.addEventListener("data", function (event) {
+                console.log(event.detail.data)
             });
-            ObserverHandler.requestData(element, new OrderBookRequest("P0", 100, "ask", "BTCUSD", "notrealtime"));
+            //ObserverHandler.requestData(element, new OrderBookRequest("P0", 100, "ask", "BTCUSD", "realtime"));
+            ObserverHandler.requestData(element, new TickerRequest("BTCUSD",1,1))
 
         };
         this.ws.onerror = function (err) {
