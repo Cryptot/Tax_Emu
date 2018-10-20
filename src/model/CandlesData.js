326b4ad014203a05099155cacf63d9db445a77a8
eh1998 <>
2018-10-20 15:20:40 +0200

    converted prototypes to es6 classes

--- src/model/CandlesData.js
+++ src/model/CandlesData.js
@@ -1,13 +1,16 @@
-function CandlesData(snapshotData) {
-    this.candles = snapshotData.slice(0, this.maxLength);
+class CandlesData {
+    constructor(snapshotData) {
+        this.candles = snapshotData.slice(0, this.maxLength);
+        this.maxLength = 60;
+    }
+
+    update(updateData) {
+        this.candles.splice(-1, 1);
+        this.candles.splice(0, 0, updateData);
+    }
+
+    static getDataFields() {
+        return ["timestamp", "open", "close", "high", "low", "volume"];
+    }
 }
 
-CandlesData.prototype.update = function (updateData) {
-    this.candles.splice(-1, 1);
-    this.candles.splice(0, 0, updateData);
-};
-
-CandlesData.getDataFields = function () {
-    return ["timestamp", "open", "close", "high", "low", "volume"];
-};
-CandlesData.prototype.maxLength = 60;
